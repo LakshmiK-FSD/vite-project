fe4985bc82a68150468e083fad99e1f814f033cc
LakshmiK-FSD <lakshmikandan2020@gmail.com>
2026-08-12 00:18:48 +0530

    learn API fetch

--- vite-project/src/MapLiat.jsx
+++ vite-project/src/MapLiat.jsx
@@ -3,86 +3,27 @@ import can from "./assets/imagepen.png"
 import hero from "./assets/hero.png"
 import panda  from "./assets/imagespanda.png"
 import Tryed from "./Try/Try.jsx"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 function MapLiat(){
-const [tempList,setTempli]=useState([{
-       id:1,
-       image:imr,
-       Name:"anil",
-       venum:true,
-       age: 2,
-       price: 1200,           
-       rating: "★★★★☆ (4.2/5)",     
-       breed: "Bengal Fox",       
-       habitat: "Forest",        
-       foodPreference: "Fruits & Nuts", 
-       availability: "Adoption" 
-
-    }
-    ,{
-           id:2,
-           image:can,
-           Name:"Nari",
-           venum:true,
-           age: 8,
-           price: 10000,           
-           rating: "★★★★☆ (4.2/5)",     
-           breed: "Bengal Fox",       
-           habitat: "Forest",        
-           foodPreference: "Fruits & Nuts", 
-           availability: "Adoption" 
-        },{    
-               id:3,
-               image:panda,
-               Name:"karadi",
-               venum:true,
-               age: 12,
-               price: 1209,           
-               rating: "★★★☆☆ (3.2/5)",     
-               breed: "Bengal Fox",       
-               habitat: "Forest",        
-               foodPreference: "Fruits & Nuts", 
-               availability: "Adoption" 
-            },{
-                  id:4,
-                  image:hero,
-                  Name:"react",
-                  venum:true,
-                  age: 9,
-                  price: 500,           
-                  rating: "★★★☆☆ (3.2/5)",     
-                  breed: "Bengal Fox",       
-                  habitat: "Forest",        
-                  foodPreference: "Fruits & Nuts", 
-                  availability: "Adoption" 
-                },{    
-                       id:5,
-                       image:imr,
-                       Name:"lakshmi",
-                       venum:true,
-                       age:4,
-                       price: 1200,           
-                       rating: "★★☆☆☆ (2.2/5)",     
-                       breed: "Bengal Fox",       
-                       habitat: "Forest",        
-                       foodPreference: "Fruits & Nuts", 
-                       availability: "Adoption" 
-                    }
-                  ]);
-                 function deleter(ider){
+   const [tempList,setTempli]=useState(null);
+   useEffect(()=>fetch("http://localhost:3000/animalsAdap").then(Rson=> {return Rson.json()}
+).then((data)=>setTempli(data)),[]);
+              
+                
+                 if(!tempList){
+return <p>looding</p>
+                 }
+                    function deleter(ider){
                     const temperary=tempList.filter((ide)=> ide.id !=ider);
-                    setTempli(temperary);
-
-                 };
-
-                    const Permen = tempList.map((callBacPar)=>
+                    setTempli(temperary);}
+                    const Permen = tempList.map((callBacPar)=>{return(
                     <Tryed key={callBacPar.id} availability={callBacPar.availability} 
                     habitat={callBacPar.habitat} foodPreference={callBacPar.foodPreference}
                      breed={callBacPar.breed} rating={callBacPar.rating} 
                      Age={callBacPar.age} price={callBacPar.price} id={callBacPar.id}
                       image={callBacPar.image} Name={callBacPar.Name} 
                      venum={callBacPar.venum} deleter = {deleter}/>
-                    );
+                    );});
     return(
         <>{
         Permen
